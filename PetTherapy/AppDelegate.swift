93cba9b08aa173900e27eb7cffd590d31f2db082
scott-lydon <scottlydon18@gmail.com>
2017-09-05 15:40:01 -0700

    I am setting the date of repeat notifications correctly.  However, it is not firing yet.

--- PetTherapy/AppDelegate.swift
+++ PetTherapy/AppDelegate.swift
@@ -2,6 +2,7 @@
 var bluePurple = UIColor(red:0.73, green:0.79, blue:0.98, alpha:1.0)
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -24,6 +25,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         bar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         application.statusBarStyle = .lightContent
         bar.isTranslucent = false
+        
+        UIApplication.shared.cancelAllLocalNotifications()
+
+            application.registerUserNotificationSettings(UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil))
+            let notificaiton = UILocalNotification()
+            let calendar = Calendar.current
+            print(Date())
+            var components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second, .nanosecond], from: Date())
+            components.minute = components.minute! + 1
+            let scheduledNotificationDate = calendar.date(from: components)
+            notificaiton.fireDate = scheduledNotificationDate!
+            notificaiton.repeatInterval = NSCalendar.Unit.day
+            notificaiton.alertBody = "It is time for your daily dose of cute!"
+            notificaiton.alertAction = "Visit Daily Dose of Cute!"
+            notificaiton.soundName = UILocalNotificationDefaultSoundName
+            UIApplication.shared.scheduleLocalNotification(notificaiton)
+   
         return true
     }
 
--- PetTherapy/ViewWillAppear.swift
+++ PetTherapy/ViewWillAppear.swift
@@ -1,6 +1,6 @@
 
 import UIKit
-import UserNotifications
+
 
 extension GiphyVC {
     
@@ -10,27 +10,16 @@ extension GiphyVC {
         
         self.navigationController?.setNavigationBarHidden(false, animated: false)
         
-        if #available(iOS 10.0, *) {
-            let center = UNUserNotificationCenter.current()
-            center.requestAuthorization(options: [.alert, .sound]) {
-                granted, error in
-            let generalCategory = UNNotificationCategory(identifier: "GENERAL", actions: [], intentIdentifiers: [], options: .customDismissAction)
-            center.setNotificationCategories([generalCategory])
-            }
-        }
-        if #available(iOS 10.0, *) {
-            let content = UNMutableNotificationContent()
-            content.title = NSString.localizedUserNotificationString(forKey: "Hello", arguments: nil)
-            content.body = NSString.localizedUserNotificationString(forKey: "Hello_message_body", arguments: nil)
-            
-            // Deliver the notificaiton in five seconds.
-            content.sound = UNNotificationSound.default()
-             let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-            let request = UNNotificationRequest(identifier: "FiveSeconds", content: content, trigger: trigger)
-             let center = UNUserNotificationCenter.current()
-            center.add(request, withCompletionHandler: nil)
-        }         
         
+//        //Test:
+//        guard let settings = UIApplication.shared.currentUserNotificationSettings else {return}
+//        if settings.types == [] {
+//            let ac = UIAlertController(title: "can't schedule", message: "either we don't have permission to schedule notifications or we haven't asked yet", preferredStyle: .alert)
+//            ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+//            self.present(ac, animated: true, completion: nil)
+//            return
+//        }
+
     }
 }
 
